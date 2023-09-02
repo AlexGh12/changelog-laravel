db17e800c800fd873e2a57f50a3187685c9cab4d
Alex G'h <a-l-e-x_1379@hotmail.com>
2023-09-01 17:41:12 -0700

    Update: Changelog Model

--- src/Models/chagelog.php
+++ src/Models/chagelog.php
@@ -4,7 +4,7 @@ namespace AlexGh12\ChangeLog\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class changelog extends Model
+class ChangeLog extends Model
 {
 	protected $table = '_changelog';
 	protected $connection = 'changelogdb';
