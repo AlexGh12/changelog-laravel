6aee1d7aab80e99a2fa57500fc53dc8f33bbd78c
OsmiDev <olopez@hdlatinoamerica.com>
2023-09-04 18:02:42 -0600

    update: comentarios comando

--- src/Commands/PublishCommand.php
+++ src/Commands/PublishCommand.php
@@ -4,16 +4,30 @@ namespace AlexGh12\ChangeLog\Commands;
 
 
 use Illuminate\Console\Command;
-
 class PublishCommand extends Command
 {
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
     protected $signature = 'ChangeLog:publish';
 
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
     protected $description = 'Publish ChangeLog configuration';
 
+
+    /**
+     * Execute the console command.
+     */
     public function handle()
     {
 		$this->call('vendor:publish', ['--tag' => 'ChangeLog:config', '--force' => true]);
-		//este es un comentario
+
     }
 }
