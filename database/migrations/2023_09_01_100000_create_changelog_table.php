2d352a934bba52aa8d2c6e5c51cdab8b1c385643
OsmiDev <olopez@hdlatinoamerica.com>
2023-09-04 18:01:55 -0600

    Add: se agrego softdelete

--- database/migrations/2023_09_01_100000_create_changelog_table.php
+++ database/migrations/2023_09_01_100000_create_changelog_table.php
@@ -27,6 +27,7 @@ return new class extends Migration
 			$table->string('title');
 			$table->text('description');
             $table->longText('content');
+			$table->softDeletes();
             $table->timestamps();
 
         });
