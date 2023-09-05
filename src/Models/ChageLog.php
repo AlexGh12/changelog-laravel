dd298f4d9074d1d5de8f187fc6a07b6241e2cab6
OsmiDev <olopez@hdlatinoamerica.com>
2023-09-04 18:03:27 -0600

    update metodos modelos

--- src/Models/ChageLog.php
+++ src/Models/ChageLog.php
@@ -3,9 +3,46 @@
 namespace AlexGh12\ChangeLog\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
 
 class ChangeLog extends Model
 {
+	use SoftDeletes;
+
 	protected $table = 'changelog';
 	protected $connection = 'changelogdb';
+	protected $fillable = [	'version','title','description','content'];
+
+	public static function fetchAll()
+    {
+        return self::all();
+    }
+
+    public static function fetchById($id)
+    {
+        return self::findOrFail($id);
+    }
+
+	public static function storeNew($data) {
+
+		$data['description'] = $data['editor_content'];
+    	return self::create($data);
+	}
+
+
+    public function modify($data)
+    {
+        $this->version     = $data['version'];
+        $this->title       = $data['title'];
+        $this->description = $data['description'];
+		$this->save();
+    }
+
+    public function remove()
+    {
+        return $this->delete();
+    }
+
+
 }
