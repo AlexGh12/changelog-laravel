da2fbe342ef8653e82dec72620c59b8000541353
OsmiDev <olopez@hdlatinoamerica.com>
2023-09-04 18:02:53 -0600

    update rutas

--- src/Http/routes.php
+++ src/Http/routes.php
@@ -1,6 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use AlexGh12\ChangeLog\Http\Controllers\VersionController;
+
+/*-------------------------------- Rutas vistas ------------------------------*/
 
 Route::get('/', 'HomeController@index')->name('changelog');
-Route::resource('/version', VersionController::class);
+Route::resource('/version', VersionController::class)->except(['show']);
+
+/*------------------------------------------------------------------------*/
+
