e15d83c7a1b5da2752fd1a4292d47f2669580877
OsmiDev <olopez@hdlatinoamerica.com>
2023-09-06 09:34:17 -0600

    Fix: se cambio el name de version

--- src/Http/routes.php
+++ src/Http/routes.php
@@ -1,12 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use AlexGh12\ChangeLog\Http\Controllers\VersionController;
 
 /*-------------------------------- Rutas vistas ------------------------------*/
 
-Route::get('/', 'HomeController@index')->name('changelog');
+Route::get('/', 'VersionController@index')->name('changelog');
 Route::resource('/version', VersionController::class)->except(['show']);
-
 /*------------------------------------------------------------------------*/
 
