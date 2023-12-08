180da480c827919d005712c5bcc3e5dd76866d30
Alex G'h <a-l-e-x_1379@hotmail.com>
2023-12-08 10:50:58 -0800

    Add conditional route registration for local environment

--- src/Http/routes.php
+++ src/Http/routes.php
@@ -3,8 +3,9 @@
 use Illuminate\Support\Facades\Route;
 
 /*-------------------------------- Rutas vistas ------------------------------*/
-
-Route::get('/', 'VersionController@index')->name('changelog');
-Route::resource('/version', VersionController::class)->except(['show']);
+if ( env('APP_ENV', 'production') == 'local' ) {
+    Route::get('/', 'VersionController@index')->name('changelog');
+    Route::resource('/version', VersionController::class)->except(['show']);
+}
 /*------------------------------------------------------------------------*/
 
